fb8bbef468097d27e1830c6cb71702abab412b49
Zach Bergman <bzach634@gmail.com>
2024-03-07 21:49:43 -0700

    add answers

--- if_statements.rb
+++ if_statements.rb
@@ -10,19 +10,19 @@
   # if it is snowy, print "coat"
   # if it is icy, print "yak traks"
 
-weather = "sunny"
+# weather = "icy"
 
-if weather == "sunny"
-  p "sunscreen"
-elsif weather == "rainy"
-  p "umbrella"
-elsif weather == "snowy"
-  p "coat"
-elsif weather == "icy"
-  p "yak traks"
-else
-  p "good to go!"
-end
+# if weather == "sunny"
+#   p "sunscreen"
+# elsif weather == "rainy"
+#   p "umbrella"
+# elsif weather == "snowy"
+#   p "coat"
+# elsif weather == "icy"
+#   p "yak traks"
+# else
+#   p "good to go!"
+# end
 
 # Experiment with manipulating the value held in variable 'weather'
 # to print something other than 'sunscreen'
@@ -41,10 +41,16 @@ end
 # Experiment with manipulating the value held within num_quarters
 # to make sure both conditions can be achieved.
 
-num_quarters = 0
+num_quarters = 1
 
-puts "I have enough money for a gumball"
-puts "I don't have enough money for a gumball"
+if num_quarters >= 2
+  puts "I have enough money for a gumball"
+else num_quarters < 2
+  puts "I don't have enough money for a gumball"
+end
+
+
+
 
 
 #####################
@@ -61,5 +67,17 @@ puts "I don't have enough money for a gumball"
 # Experiment with manipulating the value held within both variables
 # to make sure all above conditions output what you expect.
 
-cups_of_flour = 1
+cups_of_flour = 3
 has_sauce = true
+
+if cups_of_flour == 1 && has_sauce == true
+  print "I cannot make pizza"
+elsif cups_of_flour == 5 && has_sauce == false
+  print "I cannot make pizza"
+elsif cups_of_flour == 2 && has_sauce == true
+  print "I can make pizza"
+else cups_of_flour == 3 && has_sauce == true
+  print "I can make pizza"
+end
+
+
