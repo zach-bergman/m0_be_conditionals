a7be0818f7c4097052b99bf8fa0b833d50ad8c1b
Zach Bergman <bzach634@gmail.com>
2024-03-07 21:57:38 -0700

    Initial commit

---
+++ code_challenges/mild.rb
@@ -0,0 +1,11 @@
+# Write a Ruby program that defines a variable that stores an Integer. 
+# The program should print out the String "even" if the Integer is even,
+# and the String "odd" if the Integer is odd.
+
+integer = 3
+
+if integer.even?
+    p "even"
+else integer.odd?
+    p "odd"
+end
